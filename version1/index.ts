503430a2d33e0f3ca92fd6792d6ba3406e0a37c8
Ionut Nohai <nohai.ionut02@gmail.com>
2026-08-11 12:05:31 +0300

    take question from terminal

--- version1/index.ts
+++ version1/index.ts
@@ -1,13 +1,20 @@
 import { GoogleGenAI } from "@google/genai";
 
 async function main() {
+    const question = process.argv[2];
+
+    if (!question) {
+        console.error("Please provide a question")
+        return;
+    }
+
     const ai = new GoogleGenAI({
         apiKey: process.env.GEMINI_API_KEY
     })
 
     const result = await ai.interactions.create({
         model: "gemini-3.1-flash-lite",
-        input: "Explain in two sentences how AI works"
+        input: question
     })
 
     console.log(result.output_text)
