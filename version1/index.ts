220c3ac88ec74b010720a7ef106e3ec808ce2b93
Ionut Nohai <nohai.ionut02@gmail.com>
2026-08-11 12:54:35 +0300

    Model interaction build step 1

--- version1/index.ts
+++ version1/index.ts
@@ -1,5 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
+const MODEL = "gemini-3.1-flash-lite";
+
 async function main() {
     const question = process.argv[2];
 
@@ -12,13 +14,36 @@ async function main() {
         apiKey: process.env.GEMINI_API_KEY
     })
 
+    const firstInput = "My name is Bogdan";
+
     const result = await ai.interactions.create({
-        model: "gemini-3.1-flash-lite",
-        input: question
+        model: MODEL,
+        input: firstInput
     })
 
-    console.log(result.output_text)
+    console.log(result.output_text, "\n\n")
 
+    const secondInput = "What is my name?";
+
+    const result2 = await ai.interactions.create({
+        model: MODEL,
+        input: secondInput
+    })
+
+    console.log(result2.output_text, "\n\n")
+
+    const conversation = [
+        {type: "user_input" as const, content: [{type: "text" as const, text: firstInput}]}, 
+        {type: "model_output" as const, content: [{type: "text" as const, text: result.output_text || ""}]}, 
+        {type: "user_input" as const, content: [{type: "text" as const, text: secondInput}]}
+    ];
+
+    const result3 = await ai.interactions.create({
+        model: MODEL,
+        input: conversation
+    })
+
+    console.log(result3.output_text, "\n\n")
 }
 
 main()
